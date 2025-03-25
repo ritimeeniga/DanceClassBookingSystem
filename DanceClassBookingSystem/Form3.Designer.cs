aba1b838c194628aaf2385f06fed8ebbb78a65d7
ritimeeniga <riti.meeniga1@gmail.com>
2025-03-25 14:09:44 -0500

    New Edits

--- DanceClassBookingSystem/Form3.Designer.cs
+++ DanceClassBookingSystem/Form3.Designer.cs
@@ -28,19 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            linkLabel5 = new LinkLabel();
+            linkLabel4 = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            linkLabel1 = new LinkLabel();
+            label2 = new Label();
+            panel2 = new Panel();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel3 = new Panel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Silver;
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(linkLabel5);
+            panel1.Controls.Add(linkLabel4);
+            panel1.Controls.Add(linkLabel3);
+            panel1.Controls.Add(linkLabel2);
+            panel1.Controls.Add(linkLabel1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(75, 56);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(180, 468);
+            panel1.TabIndex = 1;
+            // 
+            // linkLabel5
+            // 
+            linkLabel5.AutoSize = true;
+            linkLabel5.Location = new Point(3, 442);
+            linkLabel5.Name = "linkLabel5";
+            linkLabel5.Size = new Size(67, 15);
+            linkLabel5.TabIndex = 5;
+            linkLabel5.TabStop = true;
+            linkLabel5.Text = "User Profile";
+            // 
+            // linkLabel4
+            // 
+            linkLabel4.AutoSize = true;
+            linkLabel4.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel4.Location = new Point(12, 342);
+            linkLabel4.Name = "linkLabel4";
+            linkLabel4.Size = new Size(153, 24);
+            linkLabel4.TabIndex = 4;
+            linkLabel4.TabStop = true;
+            linkLabel4.Text = "Class Catalogue";
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel3.Location = new Point(40, 264);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(107, 24);
+            linkLabel3.TabIndex = 3;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "Reminders";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel2.Location = new Point(40, 187);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(90, 24);
+            linkLabel2.TabIndex = 2;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Calendar";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.Location = new Point(12, 109);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(153, 24);
+            linkLabel1.TabIndex = 1;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Student Profiles";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(22, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(134, 25);
+            label2.TabIndex = 0;
+            label2.Text = "QUICK LINKS";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Silver;
+            panel2.Controls.Add(button6);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(-207, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(0, 0);
+            panel2.TabIndex = 6;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(405, 245);
+            button6.Name = "button6";
+            button6.Size = new Size(159, 198);
+            button6.TabIndex = 5;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(221, 243);
+            button5.Name = "button5";
+            button5.Size = new Size(159, 198);
+            button5.TabIndex = 4;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(35, 243);
+            button4.Name = "button4";
+            button4.Size = new Size(159, 198);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(405, 32);
+            button3.Name = "button3";
+            button3.Size = new Size(159, 198);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(221, 32);
+            button2.Name = "button2";
+            button2.Size = new Size(159, 198);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(35, 32);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 198);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Silver;
+            panel3.Location = new Point(309, 65);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(594, 468);
+            panel3.TabIndex = 2;
             // 
             // StudentProfiles
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(223, 211, 245);
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(975, 584);
+            Controls.Add(panel3);
+            Controls.Add(panel1);
             Name = "StudentProfiles";
             Text = "Student Profiles";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private LinkLabel linkLabel5;
+        private LinkLabel linkLabel4;
+        private LinkLabel linkLabel3;
+        private LinkLabel linkLabel2;
+        private LinkLabel linkLabel1;
+        private Label label2;
+        private Panel panel2;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Panel panel3;
     }
 }
