b1a1cb211c19d8a31d6d2785c7dc7f2419070906
ritimeeniga <riti.meeniga1@gmail.com>
2025-03-25 01:16:17 -0500

    Login Complete

--- DanceClassBookingSystem/Form1.Designer.cs
+++ DanceClassBookingSystem/Form1.Designer.cs
@@ -29,32 +29,116 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            panel1 = new Panel();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(333, 68);
+            button1.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(508, 523);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(132, 65);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "ENTER";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 50.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(434, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(260, 90);
+            label1.TabIndex = 1;
+            label1.Text = "LOGIN";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(48, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(129, 30);
+            label2.TabIndex = 2;
+            label2.Text = "Username";
+            label2.Click += label2_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Silver;
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(229, 190);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(681, 307);
+            panel1.TabIndex = 3;
+            panel1.Paint += panel1_Paint;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Microsoft JhengHei UI", 18F);
+            textBox2.Location = new Point(50, 222);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(580, 38);
+            textBox2.TabIndex = 5;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Microsoft JhengHei UI", 18F);
+            textBox1.Location = new Point(48, 98);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(580, 38);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft JhengHei UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(48, 176);
+            label3.Name = "label3";
+            label3.Size = new Size(120, 30);
+            label3.TabIndex = 3;
+            label3.Text = "Password";
+            label3.Click += label3_Click;
             // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(223, 211, 245);
+            ClientSize = new Size(1160, 673);
+            Controls.Add(panel1);
+            Controls.Add(label1);
             Controls.Add(button1);
             Name = "Login";
             Text = "Login";
             Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
     }
 }
--- DanceClassBookingSystem/Form1.cs
+++ DanceClassBookingSystem/Form1.cs
@@ -14,6 +14,49 @@ namespace DanceClassBookingSystem
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (username == "uSeRnAmE" && password == "Passw0rd!")
+            {
+                // Open the new form
+                Form2 newForm = new Form2();
+                newForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.PasswordChar = '*';
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
 
         }
     }
