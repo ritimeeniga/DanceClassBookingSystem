ff2e60db2fd69b7bebc774cc4eb44fde6bef2e13
ritimeeniga <riti.meeniga1@gmail.com>
2025-03-25 20:14:02 -0500

    Form3 Finished

--- DanceClassBookingSystem/Form3.Designer.cs
+++ DanceClassBookingSystem/Form3.Designer.cs
@@ -29,12 +29,6 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            linkLabel5 = new LinkLabel();
-            linkLabel4 = new LinkLabel();
-            linkLabel3 = new LinkLabel();
-            linkLabel2 = new LinkLabel();
-            linkLabel1 = new LinkLabel();
-            label2 = new Label();
             panel2 = new Panel();
             button6 = new Button();
             button5 = new Button();
@@ -42,9 +36,23 @@
             button3 = new Button();
             button2 = new Button();
             button1 = new Button();
+            linkLabel5 = new LinkLabel();
+            linkLabel4 = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            linkLabel1 = new LinkLabel();
+            label2 = new Label();
             panel3 = new Panel();
+            label1 = new Label();
+            listBox1 = new ListBox();
+            button7 = new Button();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            button8 = new Button();
+            button9 = new Button();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
+            panel3.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -57,74 +65,10 @@
             panel1.Controls.Add(linkLabel2);
             panel1.Controls.Add(linkLabel1);
             panel1.Controls.Add(label2);
-            panel1.Location = new Point(75, 56);
+            panel1.Location = new Point(75, 87);
             panel1.Name = "panel1";
             panel1.Size = new Size(180, 468);
             panel1.TabIndex = 1;
-            // 
-            // linkLabel5
-            // 
-            linkLabel5.AutoSize = true;
-            linkLabel5.Location = new Point(3, 442);
-            linkLabel5.Name = "linkLabel5";
-            linkLabel5.Size = new Size(67, 15);
-            linkLabel5.TabIndex = 5;
-            linkLabel5.TabStop = true;
-            linkLabel5.Text = "User Profile";
-            // 
-            // linkLabel4
-            // 
-            linkLabel4.AutoSize = true;
-            linkLabel4.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            linkLabel4.Location = new Point(12, 342);
-            linkLabel4.Name = "linkLabel4";
-            linkLabel4.Size = new Size(153, 24);
-            linkLabel4.TabIndex = 4;
-            linkLabel4.TabStop = true;
-            linkLabel4.Text = "Class Catalogue";
-            // 
-            // linkLabel3
-            // 
-            linkLabel3.AutoSize = true;
-            linkLabel3.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            linkLabel3.Location = new Point(40, 264);
-            linkLabel3.Name = "linkLabel3";
-            linkLabel3.Size = new Size(107, 24);
-            linkLabel3.TabIndex = 3;
-            linkLabel3.TabStop = true;
-            linkLabel3.Text = "Reminders";
-            // 
-            // linkLabel2
-            // 
-            linkLabel2.AutoSize = true;
-            linkLabel2.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            linkLabel2.Location = new Point(40, 187);
-            linkLabel2.Name = "linkLabel2";
-            linkLabel2.Size = new Size(90, 24);
-            linkLabel2.TabIndex = 2;
-            linkLabel2.TabStop = true;
-            linkLabel2.Text = "Calendar";
-            // 
-            // linkLabel1
-            // 
-            linkLabel1.AutoSize = true;
-            linkLabel1.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            linkLabel1.Location = new Point(12, 109);
-            linkLabel1.Name = "linkLabel1";
-            linkLabel1.Size = new Size(153, 24);
-            linkLabel1.TabIndex = 1;
-            linkLabel1.TabStop = true;
-            linkLabel1.Text = "Student Profiles";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Microsoft JhengHei UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(22, 27);
-            label2.Name = "label2";
-            label2.Size = new Size(134, 25);
-            label2.TabIndex = 0;
-            label2.Text = "QUICK LINKS";
             // 
             // panel2
             // 
@@ -194,13 +138,153 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // linkLabel5
+            // 
+            linkLabel5.AutoSize = true;
+            linkLabel5.Location = new Point(3, 442);
+            linkLabel5.Name = "linkLabel5";
+            linkLabel5.Size = new Size(67, 15);
+            linkLabel5.TabIndex = 5;
+            linkLabel5.TabStop = true;
+            linkLabel5.Text = "User Profile";
+            // 
+            // linkLabel4
+            // 
+            linkLabel4.AutoSize = true;
+            linkLabel4.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel4.Location = new Point(12, 342);
+            linkLabel4.Name = "linkLabel4";
+            linkLabel4.Size = new Size(153, 24);
+            linkLabel4.TabIndex = 4;
+            linkLabel4.TabStop = true;
+            linkLabel4.Text = "Class Catalogue";
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel3.Location = new Point(40, 264);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(107, 24);
+            linkLabel3.TabIndex = 3;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "Reminders";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel2.Location = new Point(40, 187);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(90, 24);
+            linkLabel2.TabIndex = 2;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Calendar";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.LinkVisited = true;
+            linkLabel1.Location = new Point(12, 109);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(153, 24);
+            linkLabel1.TabIndex = 1;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Student Profiles";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(22, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(134, 25);
+            label2.TabIndex = 0;
+            label2.Text = "QUICK LINKS";
+            // 
             // panel3
             // 
             panel3.BackColor = Color.Silver;
-            panel3.Location = new Point(309, 65);
+            panel3.Controls.Add(button9);
+            panel3.Controls.Add(button8);
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(button7);
+            panel3.Controls.Add(listBox1);
+            panel3.Location = new Point(309, 89);
             panel3.Name = "panel3";
             panel3.Size = new Size(594, 468);
             panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(75, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(273, 35);
+            label1.TabIndex = 3;
+            label1.Text = "STUDENT PROFILES";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.WhiteSmoke;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(16, 22);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(562, 274);
+            listBox1.TabIndex = 0;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(223, 211, 245);
+            button7.Location = new Point(16, 416);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 1;
+            button7.Text = "ADD";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Silver;
+            label3.Font = new Font("Microsoft JhengHei UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(16, 346);
+            label3.Name = "label3";
+            label3.Size = new Size(175, 22);
+            label3.TabIndex = 2;
+            label3.Text = "Enter Student Name ";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Microsoft JhengHei UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(16, 371);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(377, 33);
+            textBox1.TabIndex = 3;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(223, 211, 245);
+            button8.Location = new Point(97, 417);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 23);
+            button8.TabIndex = 4;
+            button8.Text = "SEARCH";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(223, 211, 245);
+            button9.Location = new Point(178, 417);
+            button9.Name = "button9";
+            button9.Size = new Size(75, 23);
+            button9.TabIndex = 5;
+            button9.Text = "REMOVE";
+            button9.UseVisualStyleBackColor = false;
             // 
             // StudentProfiles
             // 
@@ -208,6 +292,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(223, 211, 245);
             ClientSize = new Size(975, 584);
+            Controls.Add(label1);
             Controls.Add(panel3);
             Controls.Add(panel1);
             Name = "StudentProfiles";
@@ -215,7 +300,10 @@
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -235,5 +323,12 @@
         private Button button2;
         private Button button1;
         private Panel panel3;
+        private Label label1;
+        private Button button8;
+        private TextBox textBox1;
+        private Label label3;
+        private Button button7;
+        private ListBox listBox1;
+        private Button button9;
     }
 }
