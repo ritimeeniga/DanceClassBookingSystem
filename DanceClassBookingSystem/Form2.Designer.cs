61ae7058cc2bc7cb4f711c10caffb3d5e83988e8
ritimeeniga <riti.meeniga1@gmail.com>
2025-04-16 08:25:56 -0500

    Minor Adjustments

--- DanceClassBookingSystem/Form2.Designer.cs
+++ DanceClassBookingSystem/Form2.Designer.cs
@@ -36,13 +36,13 @@
             linkLabel1 = new LinkLabel();
             label2 = new Label();
             panel2 = new Panel();
-            button6 = new Button();
-            button5 = new Button();
-            button4 = new Button();
-            button3 = new Button();
-            button2 = new Button();
-            button1 = new Button();
             label1 = new Label();
+            button12 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
             SuspendLayout();
@@ -58,14 +58,14 @@
             panel1.Controls.Add(label2);
             panel1.Location = new Point(84, 85);
             panel1.Name = "panel1";
-            panel1.Size = new Size(180, 468);
+            panel1.Size = new Size(180, 480);
             panel1.TabIndex = 0;
             panel1.Paint += panel1_Paint;
             // 
             // linkLabel5
             // 
             linkLabel5.AutoSize = true;
-            linkLabel5.Location = new Point(3, 442);
+            linkLabel5.Location = new Point(3, 453);
             linkLabel5.Name = "linkLabel5";
             linkLabel5.Size = new Size(67, 15);
             linkLabel5.TabIndex = 5;
@@ -137,72 +137,17 @@
             // panel2
             // 
             panel2.BackColor = Color.Silver;
-            panel2.Controls.Add(button6);
             panel2.Controls.Add(button5);
             panel2.Controls.Add(button4);
             panel2.Controls.Add(button3);
             panel2.Controls.Add(button2);
             panel2.Controls.Add(button1);
+            panel2.Controls.Add(button12);
             panel2.Location = new Point(302, 85);
             panel2.Name = "panel2";
-            panel2.Size = new Size(594, 468);
+            panel2.Size = new Size(594, 480);
             panel2.TabIndex = 1;
             panel2.Paint += panel2_Paint;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(405, 245);
-            button6.Name = "button6";
-            button6.Size = new Size(159, 198);
-            button6.TabIndex = 5;
-            button6.Text = "button6";
-            button6.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(221, 243);
-            button5.Name = "button5";
-            button5.Size = new Size(159, 198);
-            button5.TabIndex = 4;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(35, 243);
-            button4.Name = "button4";
-            button4.Size = new Size(159, 198);
-            button4.TabIndex = 3;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(405, 32);
-            button3.Name = "button3";
-            button3.Size = new Size(159, 198);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(221, 32);
-            button2.Name = "button2";
-            button2.Size = new Size(159, 198);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(35, 32);
-            button1.Name = "button1";
-            button1.Size = new Size(159, 198);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
             // 
             // label1
             // 
@@ -213,6 +158,72 @@
             label1.Size = new Size(100, 35);
             label1.TabIndex = 2;
             label1.Text = "HOME";
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button12.ImageAlign = ContentAlignment.TopRight;
+            button12.Location = new Point(9, 11);
+            button12.Name = "button12";
+            button12.Size = new Size(185, 227);
+            button12.TabIndex = 6;
+            button12.Text = "JAZZ";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.ImageAlign = ContentAlignment.TopRight;
+            button1.Location = new Point(210, 11);
+            button1.Name = "button1";
+            button1.Size = new Size(185, 227);
+            button1.TabIndex = 7;
+            button1.Text = "HIP-HOP";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button2.ImageAlign = ContentAlignment.TopRight;
+            button2.Location = new Point(405, 11);
+            button2.Name = "button2";
+            button2.Size = new Size(185, 227);
+            button2.TabIndex = 8;
+            button2.Text = "BALLET";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button3.ImageAlign = ContentAlignment.TopRight;
+            button3.Location = new Point(9, 241);
+            button3.Name = "button3";
+            button3.Size = new Size(185, 227);
+            button3.TabIndex = 9;
+            button3.Text = "BALLROOM";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button4.ImageAlign = ContentAlignment.TopRight;
+            button4.Location = new Point(210, 243);
+            button4.Name = "button4";
+            button4.Size = new Size(185, 227);
+            button4.TabIndex = 10;
+            button4.Text = "coming soon...";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button5.ImageAlign = ContentAlignment.TopRight;
+            button5.Location = new Point(405, 244);
+            button5.Name = "button5";
+            button5.Size = new Size(185, 227);
+            button5.TabIndex = 11;
+            button5.Text = "coming soon...";
+            button5.UseVisualStyleBackColor = true;
             // 
             // Home
             // 
@@ -240,15 +251,15 @@
         private Label label1;
         private Label label2;
         private LinkLabel linkLabel1;
-        private Button button1;
         private LinkLabel linkLabel5;
         private LinkLabel linkLabel4;
         private LinkLabel linkLabel3;
         private LinkLabel linkLabel2;
-        private Button button6;
         private Button button5;
         private Button button4;
         private Button button3;
         private Button button2;
+        private Button button1;
+        private Button button12;
     }
 }
